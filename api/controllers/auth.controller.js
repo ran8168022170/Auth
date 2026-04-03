aab733bd6ad238695ebed94a09dd876b450b440a
Ranjeet Kanu <ranjeetkanu77@gmail.com>
2026-04-03 22:55:49 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   console.log(req.body);
@@ -19,6 +20,33 @@ export const signUp = async (req, res, next) => {
       success: true,
       message: "user created successfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "user not found"));
+    }
+
+    const validEmail = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validEmail) {
+      return next(errorHandler(404, "wrong password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httponly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignIn() {
+  const [formDada, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formDada,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  console.log(formDada);
   return (
-    <div>
-      Signin page
-      <p>dont have account</p>
-      <Link to={"/sign-up"}>
-        <span className="text-blue-500">singnUp</span>
-      </Link>
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center mt-7">SignIn</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+
+        <button className="rounded-lg border p-3 text-white bg-slate-500 hover:opacity-95 disabled:opacity-80">
+          SignIn
+        </button>
+      </form>
     </div>
   );
 }
