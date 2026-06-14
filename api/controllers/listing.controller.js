6a6f3bd0cbee2b4af8a056cc41da54b3a2945162
Ranjeet Kanu <ranjeetkanu77@gmail.com>
2026-06-14 15:45:13 +0530

    update listing api route functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,26 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "listing not exist"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "you can update your listing only"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    console.log("updateListing");
+    res.status(200).json("Listing updated successfully");
+  } catch (error) {
+    next(error);
+  }
+  res.status(200).json("listing updated successfully ");
+};
